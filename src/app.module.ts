1e70f955f9d19911176099b77cd6f950669c619a
Juan Flores <elgalleto12393@gmail.com>
2024-07-29 12:01:33 -0600

    Added services and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContactModule } from './contact/contact.module';
+import { ContacService } from './contac/contac.service';
 
 @Module({
-  imports: [],
+  imports: [ContactModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ContacService],
 })
 export class AppModule {}
