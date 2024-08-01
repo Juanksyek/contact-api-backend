cab1124425e0660a09b5e509670d1a80cb5af097
Juan Flores <elgalleto12393@gmail.com>
2024-08-01 15:42:43 -0600

    Revert netlify config

--- netlify/functions/api.ts
+++
@@ -1,6 +0,0 @@
-import { Handler, Context, Callback } from 'aws-lambda';
-import { handler } from '../../dist/src/main';
-
-export const api: Handler = (event: any, context: Context, callback: Callback) => {
-  return handler(event, context, callback);
-};
