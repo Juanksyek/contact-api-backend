d4ffff7169bc81748c5429fd7757c71d142eadbe
Juan Flores <elgalleto12393@gmail.com>
2024-08-02 11:56:22 -0600

    Deploy heroku

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
