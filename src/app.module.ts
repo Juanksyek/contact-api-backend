ebeb677190d8852153402a5e4dc4743e426ee728
Juan Flores <elgalleto12393@gmail.com>
2024-08-02 12:42:21 -0600

    Deploy heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { ContactModule } from './contact/contact.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite', // o tu tipo de base de datos
-      database: 'db.sqlite', // o la configuración de tu base de datos
+      type: process.env.DATABASE_TYPE as any || 'sqlite',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+      database: process.env.DATABASE_NAME || 'db.sqlite',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
     ContactModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
